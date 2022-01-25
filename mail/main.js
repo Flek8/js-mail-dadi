f4a94e4695e07312c5f3900eace89187bba39087
Flavio Rodolfi <flek8rodolfi@gmail.com>
2022-01-25 22:34:39 +0100

    fine

--- mail/main.js
+++ mail/main.js
@@ -1,10 +1,20 @@
 const listaMails = ['pumoiheijouzi-9999@yopmail.com','wuttoiffemmicra-9237@yopmail.com','mariorossi@gmail.com', 'xonoffobrouttei-1146@yopmail.com', 'flaviorodolfi@gmail.com', 'giorgioverdi@gmail.com', 'savexennauxe-4592@yopmail.com', 'trigretretoive-4150@yopmail.com'];
 
-let mailUtente = prompt("Qual è la tua mail?")
+let mailUtente = prompt("Qual è la tua mail?");
 
-if (listaMails.includes(mailUtente) == true) {
+let mailPresente;
+
+for (let i = 0; i < listaMails.length; i++) {
+    if (mailUtente == listaMails[i]) {
+        mailPresente = true;
+    }
+    
+}
+
+if (mailPresente == true) {
+
     alert('Accesso Consentito');
     document.getElementById('target').innerHTML = "Benvenuto nella Home Page!";
 } else {
-    alert('Accesso Negato')
+    alert('Accesso Negato');
 };
